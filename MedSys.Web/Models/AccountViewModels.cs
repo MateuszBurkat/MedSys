50a41a3ae14ec44c19b39c4791f587406fbfe598
MateuszBurkat <maburkat@outlook.com>
2020-02-14 11:07:08 +0100

    1.1

--- MedSys.Web/Models/AccountViewModels.cs
+++ MedSys.Web/Models/AccountViewModels.cs
@@ -73,14 +73,14 @@ namespace MedSys.Web.Models
         public string Email { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "{0} musi mieć co najmniej {2} znak/znaki.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "Hasło")]
+        [Display(Name = "Password")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Potwierdź hasło")]
-        [Compare("Hasło", ErrorMessage = "Hasła nie są identyczne.")]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
     }
 
